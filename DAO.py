38b1724038c63229614c948877177675ef5b2884
Jonecir Souza <jonecir@gmail.com>
2022-08-14 13:37:49 -0300

    DAO updated and finished.

--- DAO.py
+++ DAO.py
@@ -6,14 +6,20 @@ from Model import *
 class CategoryDAO:
     @classmethod
     def saveCategory(cls,cat:Category):
-        with open('categories.txt','a') as fcat:
-            fcat.writelines(cat.catName + '\n')
+        try:
+            with open('categories.txt','a') as fcat:
+                fcat.writelines(cat.catName + '\n')
+                fcat.close()
 
+        except FileNotFoundError:
+            with open('categories.txt', 'w') as fcat:
+                fcat.writelines(cat.catName + '\n')
+                fcat.close()
     
     @classmethod
     def readCategories(cls):
-        cats = []
         try:
+            cats = []
             with open("categories.txt", 'r') as fcat:
                 cls.categories = fcat.readlines()
 
@@ -25,37 +31,50 @@ class CategoryDAO:
             for i in cls.categories:
                 cats.append(i)
             
+            fcat.close()
             return cats
+
         except FileNotFoundError:
-            print("categories.txt file does not exist!")
             return cats
 
 
 class ProductDAO:
     @classmethod
     def saveProduct(cls, prod:Product):
-        with open('products.txt', 'a') as fpr:
-            fpr.writelines(prod.prodID + '|' +
-                           prod.prodName + '|' +
-                           prod.prodPrice + '|' +
-                           prod.prodCat + '\n')
+        try:
+            fpr = open('products.txt', 'a')
+
+        except FileNotFoundError:
+            fpr = open('products.txt', 'w')
+
+        fpr.writelines(prod.prodID + '|' +
+                        prod.prodName + '|' +
+                        prod.prodPrice + '|' +
+                        prod.prodCat + '\n')
+        fpr.close()
+
 
     @classmethod
     def readProducts(cls):
-        with open('products.txt', 'r') as fpr:
-            cls.products = fpr.readlines()
+        try:
+            prods = []
+            with open('products.txt', 'r') as fpr:
+                cls.products = fpr.readlines()
 
-        #replaces the \n by nothing
-        cls.products = list(
-            map(lambda x: x.replace('\n', ''), cls.products))
+            #replaces the \n by nothing
+            cls.products = list(
+                map(lambda x: x.replace('\n', ''), cls.products))
 
-        #split each sales product a list
-        cls.products = list(
-            map(lambda x: x.split('|'), cls.products))
+            #split each sales product a list
+            cls.products = list(
+                map(lambda x: x.split('|'), cls.products))
 
-        prods = []
-        for i in cls.products:
-            prods.append(Product(i[0], i[1], i[2], i[3]))
+            for i in cls.products:
+                prods.append(Product(i[0], i[1], i[2], i[3]))
+
+            fpr.close()
+        except FileNotFoundError:
+            return prods
 
         return prods
 
@@ -63,17 +82,24 @@ class ProductDAO:
 class StockDAO:
     @classmethod
     def saveStock(cls, prod: Product, stoQty):
-        with open('prodstocks.txt', 'a') as fsto:
-            fsto.writelines(prod.prodID + '|' +
-                            prod.prodName + '|' +
-                            prod.prodPrice + '|' +
-                            prod.prodCat + '|' +
-                            str(stoQty) + '\n')
+        try:
+            fsto = open('prodstocks.txt', 'a')
+
+        except FileNotFoundError:
+            fsto = open('prodstocks.txt', 'w')
+
+        fsto.writelines(prod.prodID + '|' +
+                        prod.prodName + '|' +
+                        prod.prodPrice + '|' +
+                        prod.prodCat + '|' +
+                        str(stoQty) + '\n')
+        fsto.close()
+
 
     @classmethod
     def readStock(cls):
-        stock = []
         try:
+            stocks = []
             with open('prodstocks.txt', 'r') as fsto:
                 cls.prodstocks = fsto.readlines()
 
@@ -86,48 +112,60 @@ class StockDAO:
                 map(lambda x: x.split('|'), cls.prodstocks))
 
             for i in cls.prodstocks:
-                stock.append(prodStock(Product(i[0], i[1], i[2], i[3]), int(i[4])))
+                stocks.append(prodStock(Product(i[0], i[1], i[2], i[3]), int(i[4])))
 
+            fsto.close()
         except FileNotFoundError:
-            print('prodstocks.txt file not found!')
+            return stocks
         
-        return stock
+        return stocks
 
 
 
 class SalesDAO:
     @classmethod
     def saveSales(cls, sellProd: sellProduct):
-        with open('soldprods.txt','a') as fsp:
-            fsp.writelines(sellProd.soldProd.prodID + '|' +
-                           sellProd.soldProd.prodName + '|' +
-                           sellProd.soldProd.prodPrice + '|' +
-                           sellProd.soldProd.prodCat + '|' +
-                           sellProd.salesPerson + '|' +
-                           sellProd.buyerPerson + '|' +
-                           str(sellProd.qtySold) + '|' +
-                           sellProd.dateSold + '\n')
+        try:
+            fsp = open('soldprods.txt', 'a')
+
+        except FileNotFoundError:
+            fsp = open('soldprods.txt', 'w')
+
+        fsp.writelines(sellProd.soldProd.prodID + '|' +
+                       sellProd.soldProd.prodName + '|' +
+                       sellProd.soldProd.prodPrice + '|' +
+                       sellProd.soldProd.prodCat + '|' +
+                       sellProd.salesPerson + '|' +
+                       sellProd.buyerPerson + '|' +
+                       str(sellProd.qtySold) + '\n')
+        fsp.close()
+
 
     @classmethod
     def readSoldProd(cls):
-        with open('soldprods.txt','r') as fsp:
-            cls.soldprods = fsp.readlines()
+        try:
+            sales = []
+            with open('soldprods.txt','r') as fsp:
+                cls.soldprods = fsp.readlines()
 
-        #replaces the \n by nothing
-        cls.soldprods = list(
-            map(lambda x: x.replace('\n', ''), cls.soldprods))
+            #replaces the \n by nothing
+            cls.soldprods = list(
+                map(lambda x: x.replace('\n', ''), cls.soldprods))
 
-        #print(cls.soldprods)
-        #split each sales inside a list
-        cls.soldprods = list(
-            map(lambda x: x.split('|'), cls.soldprods))
-        #print(cls.soldprods)
+            #print(cls.soldprods)
+            #split each sales inside a list
+            cls.soldprods = list(
+                map(lambda x: x.split('|'), cls.soldprods))
+            #print(cls.soldprods)
 
-        #put sales in a separate list
-        sales = []
-        for i in cls.soldprods:
-            sales.append(sellProduct(
-                Product(i[0], i[1], i[2], i[3]), i[4], i[5], i[6], i[7]))
+            #put sales in a separate list
+            for i in cls.soldprods:
+                sales.append(sellProduct(
+                    Product(i[0], i[1], i[2], i[3]), i[4], i[5], i[6], i[7]))
+
+            fsp.close()
+        except FileNotFoundError:
+            return sales
 
         return sales
 
@@ -136,28 +174,40 @@ class SalesDAO:
 class SupplierDAO:
     @classmethod
     def saveSupplier(cls, supp:Supplier):
-        with open('suppliers.txt', 'a') as fsu:
-            fsu.writelines(supp.suppName + '|' +
-                           supp.suppEIN + '|' +
-                           supp.suppPhone + '|' +
-                           supp.suppCat + '\n')
+        try:
+            fsu = open('suppliers.txt', 'a')
+
+        except FileNotFoundError:
+            fsu = open('suppliers.txt', 'w')
+
+        fsu.writelines(supp.suppEIN + '|' +
+                        supp.suppName + '|' +
+                        supp.suppTel + '|' +
+                        supp.suppCat + '\n')
+        fsu.close()
+
 
     @classmethod
     def readSuppliers(cls):
-        with open('suppliers.txt', 'r') as fsu:
-            cls.suppliers = fsu.readlines()
+        try:
+            supps = []
+            with open('suppliers.txt', 'r') as fsu:
+                cls.suppliers = fsu.readlines()
 
-        #replaces the \n by nothing
-        cls.suppliers = list(
-            map(lambda x: x.replace('\n', ''), cls.suppliers))
+            #replaces the \n by nothing
+            cls.suppliers = list(
+                map(lambda x: x.replace('\n', ''), cls.suppliers))
 
-        #split each sales inside a list
-        cls.suppliers = list(
-            map(lambda x: x.split('|'), cls.suppliers))
+            #split each sales inside a list
+            cls.suppliers = list(
+                map(lambda x: x.split('|'), cls.suppliers))
 
-        supps = []
-        for i in cls.suppliers:
-            supps.append(Supplier(i[0], i[1], i[2], i[3]))
+            for i in cls.suppliers:
+                supps.append(Supplier(i[0], i[1], i[2], i[3]))
+
+            fsu.close()
+        except FileNotFoundError:
+            return supps
 
         return supps
 
@@ -166,29 +216,41 @@ class SupplierDAO:
 class PersonDAO:
     @classmethod
     def savePerson(cls, person: Person):
-        with open('persons.txt', 'a') as fper:
-            fper.writelines(person.perName + '|' +
-                            person.perSSN + '|' +
-                            person.perEmail + '|' +
-                            person.perPhone + '|' +
-                            person.perAddress + '\n')
+        try:
+            fper = open('persons.txt', 'a')
+
+        except FileNotFoundError:
+            fper = open('persons.txt', 'w')
+
+        fper.writelines(person.perSSN + '|' +
+                        person.perName + '|' +
+                        person.perEmail + '|' +
+                        person.perPhone + '|' +
+                        person.perAddress + '\n')
+        fper.close()
+
 
     @classmethod
     def readPersons(cls):
-        with open('persons.txt', 'r') as fper:
-            cls.persons = fper.readlines()
+        try:
+            pers = []
+            with open('persons.txt', 'r') as fper:
+                cls.persons = fper.readlines()
 
-        #replaces the \n by nothing
-        cls.persons = list(
-            map(lambda x: x.replace('\n', ''), cls.persons))
+            #replaces the \n by nothing
+            cls.persons = list(
+                map(lambda x: x.replace('\n', ''), cls.persons))
 
-        #split each sales inside a list
-        cls.persons = list(
-            map(lambda x: x.split('|'), cls.persons))
+            #split each sales inside a list
+            cls.persons = list(
+                map(lambda x: x.split('|'), cls.persons))
 
-        pers = []
-        for i in cls.persons:
-            pers.append(Person(i[0], i[1], i[2], i[3], i[4]))
+            for i in cls.persons:
+                pers.append(Person(i[0], i[1], i[2], i[3], i[4]))
+
+            fper.close()
+        except FileNotFoundError:
+            return pers
 
         return pers
 
@@ -197,30 +259,43 @@ class PersonDAO:
 class EmployeeDAO:
     @classmethod
     def saveEmployee(cls, emp: Employee):
-        with open('employees.txt', 'a') as femp:
-            femp.writelines(emp.empID + '|' + 
-                            emp.perName + '|' +
-                            emp.perSSN + '|' +
-                            emp.perEmail + '|' +
-                            emp.perPhone + '|' +
-                            emp.perAddress + '\n')
+        try:
+            femp = open('employees.txt', 'a')
+
+        except FileNotFoundError:
+            femp = open('employees.txt', 'w')
+
+        femp.writelines(emp.empID + '|' + 
+                        emp.perName + '|' +
+                        emp.perSSN + '|' +
+                        emp.perEmail + '|' +
+                        emp.perPhone + '|' +
+                        emp.perAddress + '\n')
+
+        femp.close()
+
 
     @classmethod
     def readEmployees(cls):
-        with open('employees.txt', 'r') as femp:
-            cls.employees = femp.readlines()
+        try:
+            emps = []
+            with open('employees.txt', 'r') as femp:
+                cls.employees = femp.readlines()
 
-        #replaces the \n by nothing
-        cls.employees = list(
-            map(lambda x: x.replace('\n', ''), cls.employees))
+            #replaces the \n by nothing
+            cls.employees = list(
+                map(lambda x: x.replace('\n', ''), cls.employees))
 
-        #split each employee inside a list
-        cls.employees = list(
-            map(lambda x: x.split('|'), cls.employees))
+            #split each employee inside a list
+            cls.employees = list(
+                map(lambda x: x.split('|'), cls.employees))
 
-        emps = []
-        for i in cls.employees:
-            emps.append(Employee(i[0], i[1], i[2], i[3], i[4], i[5]))
+            emps = []
+            for i in cls.employees:
+                emps.append(Employee(i[0], i[1], i[2], i[3], i[4], i[5]))
+            femp.close()
+        except FileNotFoundError:
+            return emps
 
         return emps
 
@@ -230,9 +305,14 @@ class EmployeeDAO:
 #cats = CategoryDAO.readCategories()
 #for i in cats:
 #   print(i)
-#pr = Product('1', 'Apple', '5', "Fruits")
-#pr = Product('3', 'Carrots', '5', "Vegetables")
-#ProductDAO.saveProduct(pr)
+#pr1 = Product('1', 'Apple', '3', "Fruits")
+#pr2 = Product('2', 'Orange', '2', "Fruits")
+#pr3 = Product('3', 'Carrot', '5', "Veggies")
+#pr4 = Product('4', 'Potato', '1', "Vegetables")
+#ProductDAO.saveProduct(pr1)
+#ProductDAO.saveProduct(pr2)
+#ProductDAO.saveProduct(pr3)
+#ProductDAO.saveProduct(pr4)
 #pr = ProductDAO.readProducts()
 #print(pr[0].prodID, pr[0].prodName, pr[0].prodPrice, pr[0].prodCat)
 
@@ -256,15 +336,15 @@ class EmployeeDAO:
 #print(sales[0].buyerPerson)
 #print(sales[1].buyerPerson)
 
-#sup = Supplier('Wallmart', '12-3456789', '954-782-4554', 'Fruits')
+#sup = Supplier('35-3337789', 'Krogger', '857-732-5355', 'Vegetables')
 #SupplierDAO.saveSupplier(sup)
 #su = SupplierDAO.readSuppliers()
-#print(su[0].suppName, su[0].suppEIN, su[0].suppPhone, su[0].suppCat)
+#print(su[0].suppEIN, su[0].suppName, su[0].suppTel, su[0].suppCat)
 
-#person = Person('Jonecir', '383-21-2923', 'jonecir@gmail.com', '47-99148-0680', 'R. Jaboticabal, 32')
+#person = Person('383-21-2923', 'Jonecir', 'jonecir@gmail.com', '47-99148-0680', 'R. Jaboticabal, 32')
 #PersonDAO.savePerson(person)
 #per = PersonDAO.readPersons()
-#print(per[0].perName, per[0].perSSN, per[0].perEmail,
+#print(per[0].perSSN, per[0].perName, per[0].perEmail,
       #per[0].perPhone, per[0].perAddress)
 
 #empID, empName, empSSN, empEmail, empPhone, empAddress
